d9c9ffd2fda90a5887995663b9dbd80fed49dae1
Chelzea Salvador <chelzeasalvador@gmail.com>
2024-07-15 22:44:51 +0800

    add header component - desktop

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts" />
-
 <template>
-  <main>
-    <h1>Bookmark VueJS</h1>
-  </main>
+  <Layout> </Layout>
 </template>
+
+<script>
+import Layout from './components/Layout/Layout.vue'
+
+export default {
+  name: 'Bookmark',
+  components: {
+    Layout
+  }
+}
+</script>
---
+++ src/components/Layout/Header/Header.vue
@@ -0,0 +1,79 @@
+<template>
+  <header class="header-section">
+    <div class="logo-container">
+      <img
+        src="https://chlzslvdr.sirv.com/vuejs-bookmark/images/logo-bookmark.svg"
+        alt="VueJS Bookmark"
+      />
+    </div>
+    <nav class="header-menu">
+      <ul>
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><button class="login-button">Login</button></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+.header-section {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px 250px;
+  background-color: var(--white);
+}
+
+.logo-container img {
+  height: 30px;
+}
+
+.header-menu ul {
+  list-style: none;
+  display: flex;
+  gap: 60px;
+  padding: 0;
+  margin: 0;
+  text-transform: uppercase;
+  align-items: center;
+}
+
+.header-menu ul li {
+  margin: 0;
+}
+
+.header-menu ul li a {
+  text-decoration: none;
+  color: var(--neutral-dark-blue);
+  font-size: 16px;
+}
+
+.header-menu ul li a:hover {
+  color: var(--primary-soft-red);
+}
+
+.login-button {
+  background-color: var(--primary-soft-red);
+  color: var(--white);
+  border: none;
+  padding: 10px 32px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: 600;
+  text-transform: uppercase;
+}
+
+.login-button:hover {
+  background-color: var(--white);
+  color: var(--primary-soft-red);
+  border: 2px solid var(--primary-soft-red);
+}
+</style>
